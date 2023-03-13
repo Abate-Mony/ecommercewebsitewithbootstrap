0ef7afbf5824958b6532b4f6d9c5a86d994a8be4
mmanuel <bateemma14@gmail.com>
2023-03-13 13:05:54 +0100

    rm err

--- src/components/CardData.js
+++ src/components/CardData.js
@@ -5,27 +5,19 @@ export function CartProvider({ children }) {
     const [cart, setCartData] = useState([]);
     const isItemInCart = (id) => cart.includes(id)
     const addToCart = (id) => {
-        console.log(cart)
         if (!isItemInCart(id)) {
             setCartData(() => [...cart, id])
-            return true
         }
-        return false
     };
-    const getLength = cart ? .length
+    const getLength = cart.length;
     const removefromcart = (id) => {
         if (isItemInCart(id)) {
             const temp = cart.filter((item) => item !== id)
-            console.log(temp)
             setCartData(() => [...temp])
-            return true
         }
-        return false
-
     }
     return <CartContext.Provider value = {
-            { cart, setCartData, addToCart, removefromcart, isItemInCart, getLength }
-        } > { children } <
-        /CartContext.Provider>
+        { cart, setCartData, addToCart, removefromcart, isItemInCart, getLength }
+    } > { children } < /CartContext.Provider>
 
 }
